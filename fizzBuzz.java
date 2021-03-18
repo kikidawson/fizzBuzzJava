916eb4701742bcc2615c51e8a1a01d92c53842b7
Kiki Dawson <kikiadawson@gmail.com>
2021-03-18 19:31:21 +0000

    Print Fizz if 3

---
+++ fizzBuzz.java
@@ -0,0 +1,13 @@
+class FizzBuzz
+{
+  public static void main(String args[])
+  {
+    int i = 0;
+    while (i <= 100) {
+      if (i == 3) {
+        System.out.println("Fizz");
+      }
+      i += 1;
+    }
+  }
+}

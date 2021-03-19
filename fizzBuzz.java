687129fa39cf0f9c2dfb6ecda8ec60102d0a5b4d
Kiki Dawson <kikiadawson@gmail.com>
2021-03-19 13:23:55 +0000

    Print FizzBuzz if multiple of 15

--- fizzBuzz.java
+++ fizzBuzz.java
@@ -5,14 +5,16 @@ class FizzBuzz
     int i = 1;
     while (i <= 100)
     {
-      if ((i % 3) == 0) {
+      if ((i % 15) == 0) {
+        System.out.println("FizzBuzz");
+      } else if ((i % 3) == 0) {
         System.out.println("Fizz");
       } else if ((i % 5) == 0) {
         System.out.println("Buzz");
       } else {
         System.out.println(i);
       }
-      i += 1;
+      i++;
     }
   }
 }

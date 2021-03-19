d54eb87720bdc51b7886748b6c8a09b06cdd8280
Kiki Dawson <kikiadawson@gmail.com>
2021-03-19 11:49:40 +0000

    Print number if not 3

--- fizzBuzz.java
+++ fizzBuzz.java
@@ -3,9 +3,15 @@ class FizzBuzz
   public static void main(String args[])
   {
     int i = 0;
-    while (i <= 100) {
-      if (i == 3) {
+    while (i <= 100)
+    {
+      if (i == 3)
+      {
         System.out.println("Fizz");
+      }
+      else
+      {
+        System.out.println(i);
       }
       i += 1;
     }
